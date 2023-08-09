80288b395d63e47a9a88bc6f13ef199ac0a771d1
auzanassdq <auzanassidqi@gmail.com>
2023-08-09 11:43:57 +0700

    remove post query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import { MainNav } from "@/components/main-nav";
-import { Navbar } from "@/components/navbar";
-import { Sidebar } from "@/components/sidebar";
-import getAllPosts from "@/lib/getAllPost";
-import Image from "next/image";
-import { QueryClient } from "react-query";
-
-// const queryClient = new QueryClient()
-
 export default async function Home() {
-  const posts = await getAllPosts()
-
-  console.log(posts);
-
   return (
     <div className="flex flex-grow col-span-3 p-6">
-      <div>
-        Dashboard
-      </div>
+      <div>Dashboard</div>
     </div>
   );
 }
