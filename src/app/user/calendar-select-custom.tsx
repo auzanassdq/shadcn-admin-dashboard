cbb48731009562093655ad2f09ad337bc23b782d
auzanassdq <auzanassidqi@gmail.com>
2023-08-11 07:32:42 +0700

    fix calendar select custom

--- src/app/user/calendar-select-custom.tsx
+++ src/app/user/calendar-select-custom.tsx
@@ -1,48 +1,65 @@
-import { DropdownProps } from "react-day-picker";
+import { setMonth, setYear, startOfMonth } from "date-fns";
+import { DropdownProps, useNavigation } from "react-day-picker";
 
-export default function CalendarSelectCustom(props: DropdownProps) {
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface OptionProps {
+  key: string;
+  value: string;
+  children: React.ReactNode;
+}
+
+interface CalendarSelectCustomProps extends DropdownProps {
+  children?: React.ReactElement<OptionProps>[];
+}
+
+export default function CalendarSelectCustom(props: CalendarSelectCustomProps) {
+  let { goToMonth, currentMonth } = useNavigation();
+
+  const onChange = (e: any) => {
+    if (props.name == "months") {
+      let selectedMonth = Number(e);
+      let newMonth = setMonth(startOfMonth(currentMonth), selectedMonth);
+      goToMonth(newMonth);
+    }
+
+    if (props.name == "years") {
+      let newMonth = setYear(startOfMonth(currentMonth), Number(e));
+      goToMonth(newMonth);
+    }
+  };
+
   return (
-    // <Select
-    //   name={props.name}
-    //   aria-label={props["aria-label"]}
-    //   onValueChange={() => {
-    //     switch (props.name) {
-    //       case "months":
-    //         setMonth(props.value);
-    //         break;
-    //       case "years":
-    //         setYear(props.value);
-    //         break;
-    //     }
+    <>
+      <Select onValueChange={(e) => onChange(e)}>
+        <SelectTrigger id={props.name} name={props.name}>
+          <SelectValue placeholder={props.caption} />
+        </SelectTrigger>
+        <SelectContent>
+          <ScrollArea className="h-[250px] rounded-md border p-4">
+            {props.children?.map((item: any) => (
+              <SelectItem key={item.key} value={item.props.value}>
+                {item.props.children}
+              </SelectItem>
+            ))}
+          </ScrollArea>
+        </SelectContent>
+      </Select>
 
-    //     console.log(props.value);
-    //     goToMonth(new Date(year, month));
-    //   }}
-    // >
-    //   <SelectTrigger id={props.name}>
-    //     <SelectValue placeholder={props.caption} />
-    //   </SelectTrigger>
-    //   <SelectContent>
-    //     {props.children.map((item: any) => (
-    //       <SelectItem key={item.key} value={item.props.children}>
-    //         {item.props.children}
-    //       </SelectItem>
-    //     ))}
-    //   </SelectContent>
-    // </Select>
-
-    <select
-      name={props.name}
-      id={props.name}
-      placeholder={props.caption}
-      onChange={props.onChange}
-      className="bg-zinc-700 px-2 py-2"
-    >
-      {props.children.map((item: any) => (
-        <option key={item.key} value={item.props.value}>
-          {item.props.children}
-        </option>
-      ))}
-    </select>
+      {/* <select onChange={props.onChange} className="bg-zinc-700 px-2 py-2">
+        {props.children?.map((item: React.ReactElement<OptionProps>) => (
+          <option key={item.key} value={item.props.value}>
+            {item.props.children}
+          </option>
+        ))}
+      </select> */}
+    </>
   );
 }
