5cd812c9fd6674d7f71cfd00972df5e0d421447f
auzanassdq <auzanassidqi@gmail.com>
2023-08-17 18:53:38 +0700

    fix layout dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + " dark"} suppressHydrationWarning={true}>
-        <div className="flex ">
+        <div className="flex flex-row">
           <Sidebar className="hidden lg:block" />
           <Toaster />
           {children}
